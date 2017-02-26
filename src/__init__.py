6693ff91a3e95fce291e53c068ad34497ff43d01
schlerp <schlerpderpson@gmail.com>
2017-02-26 19:56:55 +0930

    readjusted fs layout, added file_signatures library

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,6 @@
-from core import ident_fsig
+# constants
+from file_signatures import signatures
+# classes
+from file_signatures import Signature, Matches, NoMatchException
+# defs
+from file_signatures import compare_sig, get_from_file, get_from_path
