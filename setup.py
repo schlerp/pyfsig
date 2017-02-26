28e24ca9b4070db38336ddc31cb63d8db423791f
schlerp <schlerpderpson@gmail.com>
2017-02-26 22:19:56 +0930

    added long description

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 
 from distutils.core import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='pyfsig',
-      version='0.2',
+      version='0.3',
       description='A python library for identifying files by headers (magic bytes)',
+      log_description=readme(),
       author='schlerp',
       author_email='schlerpderpson@gmail.com',
       url='https://github.com/schlerp/pyfsig',
