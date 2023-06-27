eec4a024552051e8f5f516ee422a159b2c8f1b6e
PattyC <patrickcoffey91@gmail.com>
2023-06-27 21:26:25 +0930

    minor updates

--- src/pyfsig/__init__.py
+++ src/pyfsig/__init__.py
@@ -1,6 +1,8 @@
 # constants
 from pyfsig.file_signatures import signatures
+
 # classes
 from pyfsig.file_signatures import Signature, Matches, NoMatchException
+
 # defs
 from pyfsig.file_signatures import compare_sig, get_from_file, get_from_path
