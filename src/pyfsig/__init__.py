d937cdc80cf7351c8353c03f39fabda3ac33ed6f
PattyC <patrickcoffey91@gmail.com>
2024-02-28 18:39:07 +0930

    Refactor: restructure the whole library
    This commit restructures the entire library to bring the exposed API up
    to date with the modern python world. This refactor is **not** backwards
    compatible and will result in a major version bump.

--- src/pyfsig/__init__.py
+++ src/pyfsig/__init__.py
@@ -1,8 +1,5 @@
-# constants
-from pyfsig.file_signatures import signatures
-
-# classes
-from pyfsig.file_signatures import Signature, Matches, NoMatchException
-
-# defs
-from pyfsig.file_signatures import compare_sig, get_from_file, get_from_path
+from pyfsig.constants import SIGNATURES  # noqa: F401
+from pyfsig.interface import (
+    find_matches_for_file_path,  # noqa: F401
+    find_matches_for_file_header,  # noqa: F401
+)
---
+++ src/tests/test_pyfsig.py
@@ -0,0 +1,32 @@
+import pyfsig
+
+
+def test_known_file_header_matches():
+    file_header = bytearray([237, 171, 238, 219])
+    print(file_header)
+    matches = pyfsig.find_matches_for_file_header(file_header)
+    assert matches
+    assert len(matches) == 1
+    assert matches[0].file_extension == "rpm"
+
+
+def test_unknown_file_header_doesnt_match():
+    file_header = bytearray([123, 123, 123, 123, 123, 123, 123])
+    print(file_header)
+    matches = pyfsig.find_matches_for_file_header(file_header)
+    assert not matches
+
+
+def test_known_file_header_matches_with_offset():
+    file_header = bytearray([0, 0, 0, 0, 102, 116, 121, 112, 51, 103])
+    matches = pyfsig.find_matches_for_file_header(file_header)
+    assert matches
+    file_exts = [match.file_extension for match in matches]
+    assert "3gp" in file_exts
+    assert "3g2" in file_exts
+
+
+def test_empty_file_header_doesnt_match():
+    file_header = bytearray()
+    matches = pyfsig.find_matches_for_file_header(file_header)
+    assert not matches
